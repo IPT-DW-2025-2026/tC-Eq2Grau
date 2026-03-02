ebbcb6c5ba1e61d146e9527bc900e6f391da7e03
jcnpereira <casimiro@ipt.pt>
2026-03-02 12:46:34 +0000

    código do controller, com uma versão 'primitiva' do algoritmo de cálculo

--- Eq2Grau/Eq2Grau/Controllers/HomeController.cs
+++ Eq2Grau/Eq2Grau/Controllers/HomeController.cs
@@ -6,7 +6,41 @@ using System.Diagnostics;
 
 namespace Eq2Grau.Controllers {
    public class HomeController:Controller {
-      public IActionResult Index() {
+
+      /// <summary>
+      /// 'porta' de entrada no programa
+      /// </summary>
+      /// <returns></returns>
+      public IActionResult Index(string A, string B, string C) {
+         /*
+          * Algoritmo:
+          * ler parâmetros A, B, C
+          * se A, B, e C forem números e A=/=0
+          *    calcular raízes
+          *    mostrar resultado x1 e x2
+          * senão, 
+          *    mostrar mensagem de erro
+          */
+
+
+         // validar se A, B, e C forem números e A=/=0
+         // if(!float.TryParse(A, out float a)) { }
+
+         float.TryParse(A, out float a);
+         float.TryParse(B, out float b);
+         float.TryParse(C, out float c);
+
+         // Calcular Raízes
+         float delta = b*b-4*a*c;
+
+         float x1 = (-b + MathF.Sqrt(delta))/(2*a);
+         float x2 = (-b - MathF.Sqrt(delta))/(2*a);
+
+         // enviar os dados calculados para a View
+         ViewBag.X1 = x1;
+         ViewBag.X2 = x2;
+
+
          return View();
       }
 
